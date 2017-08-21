57944d6912b2895098c82d1d54f88ab185822687
yywatanabe <yuya.eth01@gmail.com>
2017-08-21 12:02:18 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,5 +12,7 @@ module RailsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
